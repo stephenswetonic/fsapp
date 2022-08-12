785137863e3cd80c89b91a2f093d4a41cd0e8384
stephenswetonic <sswetonc@uw.edu>
2022-08-11 19:38:48 -0700

    Added boolean finished field to Fsjob

--- fswebsite/fsapp/views.py
+++ fswebsite/fsapp/views.py
@@ -42,45 +42,48 @@ def fsmain(request, id):
 
   taskid_img_dict = {}
 
-  new_dir = str(settings.MEDIA_ROOT) + '/job' + str(id)
-  if not os.path.exists(new_dir):
-    os.mkdir(new_dir)
+  if not job.is_finished:
+    new_dir = str(settings.MEDIA_ROOT) + '/job' + str(id)
+    if not os.path.exists(new_dir):
+      os.mkdir(new_dir)
 
-  # Create a list of cv2 images to be aligned
-  for i in range(len(images)):
-    path = str(settings.MEDIA_ROOT) + '/' + images[i].image.name
-    print(path)
-    cv2_imgs.append(cv2.imread(str(path)))
-  
-  
-  # Create x test files for each filterimage
-  for i in range(len(images)):
-    path = new_dir + ('/filterimage' + str(i) + '.png')
-    img = File(open(path, 'w'))
-    filter_img_paths.append(path)
-    filter_img_path_short.append(('/images/job' + str(id) + '/filterimage' + str(i) + '.png'))
+    # Create a list of cv2 images to be aligned
+    for i in range(len(images)):
+      path = str(settings.MEDIA_ROOT) + '/' + images[i].image.name
+      print(path)
+      cv2_imgs.append(cv2.imread(str(path)))
+    
+    
+    # Create x test files for each filterimage
+    for i in range(len(images)):
+      path = new_dir + ('/filterimage' + str(i) + '.png')
+      img = File(open(path, 'w'))
+      filter_img_paths.append(path)
+      filter_img_path_short.append(('/images/job' + str(id) + '/filterimage' + str(i) + '.png'))
 
-  # Create paths for each aligned image
-  for i in range(len(images)):
-    path = new_dir + ('/alignedimage' + str(i) + '.png')
-    img = File(open(path, 'w'))
-    aligned_img_paths.append(path)
+    # Create paths for each aligned image
+    for i in range(len(images)):
+      path = new_dir + ('/alignedimage' + str(i) + '.png')
+      img = File(open(path, 'w'))
+      aligned_img_paths.append(path)
 
-  # Align
-  aligned_imgs = align_images(cv2_imgs)
+    # Align
+    aligned_imgs = align_images(cv2_imgs)
 
-  # imwrite to aligned img paths
-  for i in range(len(aligned_img_paths)):
-    cv2.imwrite(aligned_img_paths[i], aligned_imgs[i])
+    # imwrite to aligned img paths
+    for i in range(len(aligned_img_paths)):
+      cv2.imwrite(aligned_img_paths[i], aligned_imgs[i])
 
-  # Start tasks
-  for i in range(len(images)):
-    task = focus_stack.delay(aligned_img_paths[i], str(filter_img_paths[i]))
-    taskid_img_dict.update({task.id : filter_img_path_short[i]})
-    celery_tasks.append(task)
-    celery_task_ids.append(task.id)
+    # Start tasks
+    for i in range(len(images)):
+      task = focus_stack.delay(aligned_img_paths[i], str(filter_img_paths[i]))
+      taskid_img_dict.update({task.id : filter_img_path_short[i]})
+      celery_tasks.append(task)
+      celery_task_ids.append(task.id)
 
-  return render(request, 'fsapp/fsmain.html', {'images' : images, 'task_ids' : celery_task_ids, 'job_id':id, 'taskid_img_dict': json.dumps(taskid_img_dict) })
+    return render(request, 'fsapp/fsmain.html', {'images' : images, 'task_ids' : celery_task_ids, 'job_id':id, 'taskid_img_dict': json.dumps(taskid_img_dict) })
+  else:
+    return render(request, 'fsapp/fsmain_loading.html', {'jobid' : id})
 
 # This function gets pinged until all images have loaded (all celery tasks done)
 def fsmain_loading(request, id):
@@ -151,6 +154,8 @@ def fsmain_loaded(request, id):
     background.paste(Image.open(filtered_images_cv2[i+1]), (0,0), mask=Image.open(filtered_images_cv2[i+1]))
 
   background.save(result_path)
+  job.is_finished = True
+  job.save()
 
   return render(request, 'fsapp/fsmain_loaded.html', {'images' : images, 'filtered_images': filtered_images, 'result' : result_path_template})
 
